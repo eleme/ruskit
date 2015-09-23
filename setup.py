4f38d795558fd73b9770d555d834478183b9a6e6
maralla <maralla.ai@gmail.com>
2015-09-23 18:37:54 +0800

    add license file

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 
 setup(
     name="ruskit",
     version="0.0.1",
     description="Redis cluster administration toolkit",
+    long_description=long_description,
+    url="https://github.com/eleme/ruskit",
+    author="maralla",
+    author_email="imaralla@icloud.com",
+    license="MIT",
+    keyswords="redis cluster administration",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["ruskit = ruskit.cmds:main"]
@@ -16,4 +25,13 @@ setup(
         "hiredis",
         "redis"
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ]
 )
