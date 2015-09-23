b7bb66e9887387a1cbb291593c8355c5d6d0de2d
maralla <maralla.ai@gmail.com>
2015-09-23 17:44:19 +0800

    add travis ci

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,13 @@
+import collections
+
 from ruskit.utils import spread
 
 
 def test_spread():
-    data = {
-        "192.168.0.1": [1, 2],
-        "192.168.0.2": [3, 4],
-        "192.168.0.3": [5, 6]
-    }
+    data = collections.OrderedDict()
+    data["192.168.0.1"] = [1, 2]
+    data["192.168.0.2"] = [3, 4]
+    data["192.168.0.3"] = [5, 6]
 
     res = spread(data, 4)
     assert res == [1, 3, 5, 2]
